52cedf7f6bd066959c761036d25f77b82763c48b
prodigg <raphael@familie-sauer.ch>
2026-03-07 20:10:08 +0100

    implemented data for storing metadata in the ProcessDataBuffer

--- src/ProcessDataBuffer.cpp
+++ src/ProcessDataBuffer.cpp
@@ -6,18 +6,35 @@
 #include <stdexcept>
 
 void ProcessDataBuffer_t::setSymbolValue(const std::string &value, const std::string &symbolName) {
-    std::scoped_lock lock (dataAccess);
-    if (symbolsValues.contains(symbolName))
-        symbolsValues.at(symbolName) = value;
-    else
-        symbolsValues.emplace(symbolName, value);
+    setSymbolData(symbolName, {
+        std::chrono::milliseconds(0),
+        std::chrono::milliseconds(0),
+        std::chrono::system_clock::now(),
+        true,
+        value
+    });
 }
 
 void ProcessDataBuffer_t::getSymbolValue(const std::string &symbolName, std::string &value) {
+    symbolData_t data;
+    getSymbolData(symbolName, data);
+    value = data.symbolValue;
+}
+
+void ProcessDataBuffer_t::setSymbolData(const std::string& symbolName, symbolData_t data) {
+    std::scoped_lock lock (dataAccess);
+    if (symbolsValues.contains(symbolName))
+        symbolsValues.at(symbolName) = data;
+    else
+        symbolsValues.emplace(symbolName, data);
+}
+
+void ProcessDataBuffer_t::getSymbolData(const std::string& symbolName, symbolData_t& data) {
     std::scoped_lock lock (dataAccess);
     try {
-        value = symbolsValues.at(symbolName);
+        data = symbolsValues.at(symbolName);
     } catch (std::out_of_range &ex) {
-        value = "NaN";
+        data.symbolValue = "NaN";
+        data.wasLastReadSuccessful = false;
     }
 }
--- src/ProcessDataBuffer.h
+++ src/ProcessDataBuffer.h
@@ -8,6 +8,15 @@
 #include <mutex>
 #include <string>
 #include <unordered_map>
+#include <chrono>
+
+struct symbolData_t {
+    std::chrono::steady_clock::duration lastCurrentTime; // Time between last and current read
+    std::chrono::steady_clock::duration symbolReadTime; // Time to read the symbol
+    std::chrono::system_clock::time_point lastReadTime;
+    bool wasLastReadSuccessful = false; // true if last read attempt was successfull
+    std::string symbolValue;
+};
 
 /*!
  * @brief this class is a storage container between ADS receiver and REST endpoint
@@ -16,17 +25,33 @@
 class ProcessDataBuffer_t {
 public:
     /*!
+     *
+     * @param symbolName the symbol name
+     * @param data the Data being inserted into the symbol name
+     */
+    void setSymbolData(const std::string& symbolName, symbolData_t data);
+
+    /*!
+     *
+     * @param symbolName the symbol name
+     * @param data [OUT] the data that is at the symbol name
+     */
+    void getSymbolData(const std::string& symbolName, symbolData_t& data);
+
+    /*!
      * @param value the value that is inserted in the symbol name
      * @param symbolName the symbol name
      */
+    [[deprecated]]
     void setSymbolValue(const std::string &value, const std::string &symbolName);
 
     /*!
      * @param symbolName the name of the symbol to get
      * @param value [OUT] the value that is at the symbol name
      */
+    [[deprecated]]
     void getSymbolValue(const std::string &symbolName, std::string& value);
 private:
     std::mutex dataAccess;
-    std::unordered_map<std::string, std::string> symbolsValues;
+    std::unordered_map<std::string, symbolData_t> symbolsValues;
 };
